66552cff3dae8dfecfd9aa8c21f378a48adc7940
Alison Tahiri <alison.tahiri@gmail.com>
2026-03-06 14:45:33 +0100

    feat: set up initial Capacitor Android project including app structure, resources, and build configurations.

---
+++ android/app/src/main/java/com/duitche/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.duitche.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.duitche.app',
+  appName: 'Deutchi',
+  webDir: 'dist'
+};
+
+export default config;
